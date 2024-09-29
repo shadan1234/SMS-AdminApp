d31869b65f0a76c1b087b8e36e04584d496a51af
shadan <shadan.hussain.2004@gmail.com>
2024-09-29 12:29:21 +0530

    fix:http

--- lib/constants/global.dart
+++ lib/constants/global.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void showSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
 }
-String uri = 'hthttps://sms-admin-app.vercel.app';
+String uri = 'https://sms-admin-app.vercel.app';
 class GlobalVariables{
 
 }
